57acb588d4b5fef9c40435c7440b73bd5e0b307e
antigravkids <hcarter333@gmail.com>
2023-01-09 18:53:18 -0800

    grep and tail version
    Works by sending output to file, and then using 'tail -f' of file fed into grep in a different terminal to gather more specific real-time data.

--- rbntelnet.py
+++ rbntelnet.py
@@ -1,10 +1,20 @@
 import getpass
 import telnetlib
 import time
+import sys
+import signal
 
 HOST = "telnet.reversebeacon.net"
 
+def signal_handler(sig, frame):
+    print('You pressed Ctrl+C!')
+    tn.close()
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
+print("Opening rnb tn");
 tn = telnetlib.Telnet(HOST, 7000)
+
 print(tn)
 print("About to call read_until\n")
 tn.read_until(b"Please enter your call: ", 17)
@@ -14,11 +24,6 @@ print("Callsign sent\n");
 
 #for x in range(2):
 #print(tn.read_until(b"DX de", 5).decode('ascii'))
+print("starting interact")
 tn.interact()
-time.sleep(15.5)
-#print(tn.read_eager().decode('ascii'))
-#print(tn.read_until(b"DX de", 5).decode('ascii'))
-
-tn.close()
-
-#DX de DL1HWS-#:  14033.0  HZ1UAE         CW     7 dB  28 WPM
+print("done with interact")
